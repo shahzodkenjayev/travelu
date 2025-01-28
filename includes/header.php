9216dcfb21c832aa4b1fccee17c238edd85e614b
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-28 13:55:38 +0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,12 @@
 <div class="header_wrapper">
     <a href="index.php"><img id="logo" width="200" height="200" src="images/logo1.jpg"></a>
     <img id="banner" width="800" height="300" src="images/unitur.webp">
+
+    <!-- Bayroqlarni qo'shish -->
+    <div class="language_flags">
+        <a href="?lang=uz"><img src="images/flag_uzbek.png" alt="O'zbekcha"></a>
+        <a href="?lang=en"><img src="images/flag_english.png" alt="English"></a>
+        <a href="?lang=ru"><img src="images/flag_russian.png" alt="Русский"></a>
+        <a href="?lang=en"><img src="images/flag_turkey.png" alt="Turkey"></a>
+    </div>
 </div>
