79d1455047e0dec0ae1d4f21612546b355081ca7
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-28 13:59:38 +0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,4 @@
+<link rel="icon" href="favicon.png" type="image/x-icon">
 <div class="header_wrapper">
     <a href="index.php"><img id="logo" width="200" height="200" src="images/logo1.jpg"></a>
     <img id="banner" width="800" height="300" src="images/unitur.webp">
