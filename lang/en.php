885abb0af84b2c5f5e8615ebd20479e9a7ffb1f7
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-29 10:45:25 +0500

    Create en.php

---
+++ lang/en.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'Xush kelibsiz' => 'Welcome',
+    'your' => 'Your',
+    'selected' => 'selected tours',
+    'total_price' => 'Total price',
+    'cart_return' => 'Return to cart',
+    'personal_account' => 'Personal account',
+    'Chiqish' => 'Logout',
+    'Savatcha ' => 'Shopping cart',
+    'Biz bilan bog'lanish' => 'Contact us',
+    'services' => 'Our services',
+    // boshqa matnlar...
+];
+?>
