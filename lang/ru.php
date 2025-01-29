25fb3a2b99d3d4b396bcf2e4d3c805fd19b381d1
shahzodkenjayev <shahzodkenjayevyangiboyevich@gmail.com>
2025-01-29 05:38:31 +0000

    Add ru.php and en.php translation files

---
+++ lang/ru.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'welcome' => 'Добро пожаловать',
+    'your' => 'Ваши',
+    'selected' => 'выбранные туры',
+    'total_price' => 'Общая стоимость',
+    'cart_return' => 'Вернуться в корзину',
+    'personal_account' => 'Личный кабинет',
+    'logout' => 'Выход',
+    'shopping_cart' => 'Корзина',
+    'contact_us' => 'Связаться с нами',
+    'services' => 'Наши услуги',
+    // boshqa matnlar...
+];
+?>
