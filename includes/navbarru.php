0f0c959c08215c1dcc3b56fbd6a6481be212da69
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-31 13:45:54 +0500

    Create navbarru.php

---
+++ includes/navbarru.php
@@ -0,0 +1,16 @@
+<div class="menubar">
+    <ul id="menu">
+        <li><a href="index.php">Домашняя страница</a></li>
+        <li><a href="all_packages.php">Все пакеты</a></li>
+        <li><a href="checkout.php">Мой счет</a></li>
+        <li><a href="customer_register.php">Регистраться</a></li>
+        <li><a href="cart.php">Корзина</a></li>
+        <li><a href="contact.php">Связь</a></li>
+    </ul>
+    <div id="form">
+        <form method="get" action="results.php" enctype="multipart/form-data">
+            <input type="text" name="user_query" placeholder="ПОИСК">
+            <input id="search" type="submit" name="search" value="Найти">
+        </form>
+    </div>
+</div>
