fe03e491db3dd25b0292c5ef728f826edca0f393
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-31 08:56:44 +0000

    Create headerru.php

---
+++ includes/headerru.php
@@ -0,0 +1,14 @@
+
+<link rel="icon" href="favicon.png" type="image/x-icon">
+<div class="header_wrapper">
+    <a href="index.php"><img id="logo" width="200" height="200" src="images/logo1.jpg"></a>
+    <img id="banner" width="800" height="300" src="images/unitur.webp">
+
+    <!-- Bayroqlarni qo'shish -->
+    <div class="language_flags">
+        <a href="?lang=uz"><img src="images/flag_uzbek.png" alt="O'zbekcha"></a>
+        <a href="indexen.php"><img src="images/flag_english.png" alt="English"></a>
+        <a href="indexru.php"><img src="images/flag_russian.png" alt="Русский"></a>
+        <a href="indextr.php"><img src="images/flag_turkey.png" alt="Turkey"></a>
+    </div>
+</div>
