88193c3a74d61ee128ef29f806448b0f61dcc214
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-31 09:21:02 +0000

    Create left-sidebaren.php

---
+++ includes/left-sidebaren.php
@@ -0,0 +1,12 @@
+
+<div class="sidebar">
+    <div id="sidebar_title"><b>KATEGORIYA</b></div>
+    <ul id="cats">
+        <?php getCats(); ?>
+    </ul>
+    <br>
+    <div id="sidebar_title"><b>TURLARI</b></div>
+    <ul id="cats">
+        <?php getTypes(); ?>
+    </ul>
+</div>
