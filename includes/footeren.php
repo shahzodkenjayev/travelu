a5ec640995de70426487f17d968a730a957ad816
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-31 13:49:01 +0500

    Create footeren.php

---
+++ includes/footeren.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Follow Us</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body id="body">
+    <div class="main_wrapper">
+        <div class="header_wrapper">
+            
+        </div>
+        <div class="menubar">
+            <ul id="menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        <div class="content_wrapper container">
+            <div id="content_area">
+                <h2>Our Mission</h2>
+                <p>“Unitur” is an international travel agency, a reliable leader in tourism and medical tourism. 
+                   We make our clients' dreams come true.</p>
+            </div>
+            <div class="sidebar">
+            <h3>Follow Us</h3>
+					<ul>
+    <li><i class="fa-brands fa-telegram"></i><a href="https://t.me/shahzodkenjayev" class="fb"> Telegram</a></li>
+    <li><i class="fa-brands fa-instagram"></i><a href="https://www.instagram.com/unitouruz" class="insta"> Instagram</a></li>
+    <li><a href="https://profiuniversity.uz" class="gplus"> Profiuniversity</a></li>
+</ul>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <p>&copy; 2025 unitur.uz. All rights reserved.</p>
+    </div>
+</body>
+</html>
